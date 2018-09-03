04182a2601048c48abba48153819e40780c90f4a
Alexander Ivanov <sashko99@hotmail.com>
2018-09-03 23:44:52 +0300

    Pattern matcher refactory

--- src/JavaIntroduction/PatternMatcher/src/PatternMatcher.java
+++ src/JavaIntroduction/PatternMatcher/src/PatternMatcher.java
@@ -1,61 +1,67 @@
 import java.util.Scanner;
 
 public class PatternMatcher {
-    static String text, pattern;
+    private static String text, pattern;
+    private static Boolean[][] matchCache;
 
-    static Boolean[][] memz;
-
-    public  static  void main(String[] args){
+    public static void main(String[] args) {
         String[] line = new Scanner(System.in).nextLine().split("([ ])");
         text = line[0];
         pattern = trim(line[1], '*');
 
-        memz=new Boolean[text.length()+1][pattern.length()+1];
+        matchCache = new Boolean[text.length() + 1][pattern.length() + 1];
 
-        for(int i=0;i<text.length();i++){
-            if(match(i, 0)){
-                System.out.println("true");
-                return;
-            }
+        boolean answer = false;
+        for (int i = 0; i < text.length() && !answer; i++) {
+            answer = answer || (match(i, 0));
         }
 
-        System.out.println("false");
+        System.out.println(answer);
     }
 
-    static String trim(String str, char c){
-        int firstNonStar = 0, lastNonStar = str.length()-1;
-        while (firstNonStar<str.length() && str.charAt(firstNonStar)=='*')firstNonStar++;
-        while (lastNonStar>=0 && str.charAt(lastNonStar)=='*')lastNonStar--;
+    static String trim(String str, char c) {
+        int firstNonStar = 0, lastNonStar = str.length() - 1;
+        while (firstNonStar < str.length() && str.charAt(firstNonStar) == '*') firstNonStar++;
+        while (lastNonStar >= 0 && str.charAt(lastNonStar) == '*') lastNonStar--;
 
-        if(firstNonStar>=str.length() || firstNonStar>lastNonStar)return  "";
+        if (firstNonStar >= str.length() || firstNonStar > lastNonStar) return "";
         return str.substring(firstNonStar, lastNonStar + 1);
     }
 
-    public  static  boolean match(int textIndex, int patternIndex){
-        if(patternIndex>=pattern.length())return  true;
-        if(textIndex >= text.length()){
-            for(int i=patternIndex;i<pattern.length();i++) {
-                if (pattern.charAt(i) != '*') return memz[textIndex][patternIndex] = false;
+    public static boolean match(int textIndex, int patternIndex) {
+        if (patternIndex >= pattern.length()) {
+            return matchCache[textIndex][patternIndex] = true;
+        }
+        if (textIndex >= text.length()) {
+            for (int i = patternIndex; i < pattern.length(); i++) {
+                if (pattern.charAt(i) != '*') return matchCache[textIndex][patternIndex] = false;
             }
-            return memz[textIndex][patternIndex]=true;
+            return matchCache[textIndex][patternIndex] = true;
         }
 
-        if(memz[textIndex][patternIndex]!=null)return  memz[textIndex][patternIndex].booleanValue();
+        if (matchCache[textIndex][patternIndex] != null) {
+            return matchCache[textIndex][patternIndex].booleanValue();
+        }
 
-        if(pattern.charAt(patternIndex) == '?'){
-            return memz[textIndex][patternIndex] =   match(textIndex + 1, patternIndex + 1);
-        }
-        else if(pattern.charAt(patternIndex) == '*'){
-            for(int i=textIndex; i<text.length();i++){
-                if(match(i, patternIndex + 1))return memz[textIndex][patternIndex] =  true;
+        if (pattern.charAt(patternIndex) == '?') {
+            return (
+                matchCache[textIndex][patternIndex] =
+                    match(textIndex + 1, patternIndex + 1)
+            );
+        } else if (pattern.charAt(patternIndex) == '*') {
+            for (int i = textIndex; i < text.length(); i++) {
+                if (match(i, patternIndex + 1)) {
+                    return matchCache[textIndex][patternIndex] = true;
+                }
             }
-            return memz[textIndex][patternIndex] =  false;
-        }
-        else{
-            return memz[textIndex][patternIndex] =
-                    pattern.charAt(patternIndex) == text.charAt(textIndex)
-                            &&
-                    match(textIndex+1, patternIndex + 1);
+            return matchCache[textIndex][patternIndex] = false;
+        } else {
+            return (
+                matchCache[textIndex][patternIndex]
+                    =
+                    pattern.charAt(patternIndex) == text.charAt(textIndex) &&
+                        match(textIndex + 1, patternIndex + 1)
+            );
         }
     }
 }
